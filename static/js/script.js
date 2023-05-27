61b4909d087bbd6d84979269804c0538f3d5a351
nurultaukid13 <nurultaukid17@gmail.com>
2023-05-27 20:42:40 +0700

    gas

--- static/js/script.js
+++ static/js/script.js
@@ -1,46 +1,48 @@
-const uploadDrop = document.querySelector('.upload-drop');
-const uploadInput = document.getElementById('upload');
+document.addEventListener('DOMContentLoaded', () => {
+    const uploadDrop = document.querySelector('.upload-drop');
+    const uploadInput = document.getElementById('upload');
 
-// Handle file drop
-uploadDrop.addEventListener('drop', (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append('upload', event.dataTransfer.files[0]);
+    // Handle file drop
+    uploadDrop.addEventListener('drop', (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('upload', event.dataTransfer.files[0]);
 
-    // Send file to server
-    fetch('/upload/sketch', {
-        method: 'POST',
-        body: formData
-    }).then(response => {
-        if (response.ok) {
-            alert('Gambar berhasil diupload!');
-            window.location.href = '/uploaded_sketch';
-        } else {
-            alert('Terjadi kesalahan saat upload gambar. Pastikan mengupload dengan format yang diizinkan (png, jpg atau jpeg) dan ukuran dibawah 10MB');
-        }
+        // Send file to server
+        fetch('/upload/sketch', {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            if (response.ok) {
+                alert('Gambar berhasil diupload!');
+                window.location.href = '/uploaded_sketch';
+            } else {
+                alert('Terjadi kesalahan saat upload gambar. Pastikan mengupload dengan format yang diizinkan (png, jpg atau jpeg) dan ukuran dibawah 10MB');
+            }
+        });
     });
-});
 
-// Prevent default behavior when file is dragged over uploadDrop
-uploadDrop.addEventListener('dragover', (event) => {
-    event.preventDefault();
-});
+    // Prevent default behavior when file is dragged over uploadDrop
+    uploadDrop.addEventListener('dragover', (event) => {
+        event.preventDefault();
+    });
 
-// Handle file selection through uploadInput
-uploadInput.addEventListener('change', (event) => {
-    const formData = new FormData();
-    formData.append('upload', event.target.files[0]);
+    // Handle file selection through uploadInput
+    uploadInput.addEventListener('change', (event) => {
+        const formData = new FormData();
+        formData.append('upload', event.target.files[0]);
 
-    // Send file to server
-    fetch('/upload/sketch', {
-        method: 'POST',
-        body: formData
-    }).then(response => {
-        if (response.ok) {
-            alert('Gambar berhasil diupload!');
-            window.location.href = '/uploaded_sketch';
-        } else {
-            alert('Terjadi kesalahan saat upload gambar.  Pastikan mengupload dengan format yang diizinkan (png, jpg atau jpeg) dan ukuran dibawah 10MB');
-        }
+        // Send file to server
+        fetch('/upload/sketch', {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            if (response.ok) {
+                alert('Gambar berhasil diupload!');
+                window.location.href = '/uploaded_sketch';
+            } else {
+                alert('Terjadi kesalahan saat upload gambar.  Pastikan mengupload dengan format yang diizinkan (png, jpg atau jpeg) dan ukuran dibawah 10MB');
+            }
+        });
     });
 });
