d3105061fe403def198211cf9e7ef848d892612b
nurultaukid13 <nurultaukid17@gmail.com>
2023-06-04 11:18:38 +0700

    done

--- webapp.py
+++ webapp.py
@@ -4,6 +4,7 @@ from image import Image
 from color import Color
 from filemanager import FileManager
 
+
 class WebApp:
     def __init__(self):
         self.app = Flask(__name__)
@@ -53,34 +54,41 @@ class WebApp:
                 gambar.set_weight_before_compress()
                 file_weight = gambar.get_weight_before_compress()
                 gambar.file_path_download = sketch_image
-                return render_template('upload.html', file_path = gambar.file_path_download, file_weight = round(file_weight/1000))
-            
+                return render_template('upload.html', file_path=gambar.file_path_download, file_weight=round(file_weight/1000))
+
         @self.app.route('/uploaded_sketch/coloring', methods=['POST'])
         def proces_color():
             warna.set_rgb_color(request.form.get('color'))
             return redirect(url_for('color_page'))
-        
+
         @self.app.route('/uploaded_coloring')
         def color_page():
-            colorsketch=warna.hex_to_rgb(warna.get_rgb_color())
-            self.facade.set_color(self.facade.get_sketch_image(), colorsketch)
-            self.filemanager.delete_files_in_folder_after_timeout('coloring',60)
-            gambar.set_file_path_after_compress(gambar.get_file_name(), 'static/coloring')
-            gambar.set_weight_after_compress()
-            file_weight = gambar.get_weight_after_compress()
-            gambar.file_path_download = self.facade.get_coloring_image()
-            return render_template('upload.html', file_path=gambar.file_path_download, file_weight = round(file_weight/1000))
+            colorsketch = warna.hex_to_rgb(warna.get_rgb_color())
+            if colorsketch==(255,255,255):
+                gambar.file_path_download = self.facade.get_sketch_image()
+                gambar.set_file_path_before_compress( gambar.get_file_name(), 'static/sketch')
+                gambar.set_weight_before_compress()
+                file_weight = gambar.get_weight_before_compress()
+                warna.set_rgb_color(colorsketch)
+            else:
+                self.facade.set_color(self.facade.get_sketch_image(), colorsketch)
+                self.filemanager.delete_files_in_folder_after_timeout('coloring', 60)
+                gambar.set_file_path_before_compress( gambar.get_file_name(), 'static/coloring')
+                gambar.set_weight_before_compress()
+                file_weight = gambar.get_weight_before_compress()
+                gambar.file_path_download = self.facade.get_coloring_image()
+            return render_template('upload.html', file_path=gambar.file_path_download, file_weight = round(file_weight/1000), color=colorsketch)
             
         @self.app.route('/download', methods=['GET'])
         def download_file():
             compress_checkbox = request.args.get('compress') == 'on'
             if compress_checkbox:
-                if self.facade.get_coloring_image() and self.facade.get_coloring_image() == 'static/coloring/'+gambar.file_name:
-                    gambar.file_path_download = self.facade.compress_image(self.facade.get_coloring_image(), max_size=1920)
+                if self.facade.get_coloring_image() and self.facade.get_coloring_image() == 'static/coloring/'+gambar.file_name and warna.get_rgb_color()!=(255,255,255):
+                    gambar.file_path_download = self.facade.compress_image(self.facade.get_coloring_image(), max_size=1080)
                 else:
-                    gambar.file_path_download = self.facade.compress_image(self.facade.get_sketch_image(), max_size=1920)
+                    gambar.file_path_download = self.facade.compress_image(self.facade.get_sketch_image(), max_size=1080)
             else:
-                if self.facade.get_coloring_image() and self.facade.get_coloring_image() == 'static/coloring/'+gambar.file_name:
+                if self.facade.get_coloring_image() and self.facade.get_coloring_image() == 'static/coloring/'+gambar.file_name and warna.get_rgb_color()!=(255,255,255):
                     gambar.file_path_download = self.facade.get_coloring_image()
                 else:
                     gambar.file_path_download = self.facade.get_sketch_image()
